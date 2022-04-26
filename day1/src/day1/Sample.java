5d0902e2d450737a5388d4c3be938ed307368ad1
azerty0301 <morozumiyumie@gmail.com>
2022-04-26 14:26:10 +0900

    Ec

--- day1/src/day1/Sample.java
+++ day1/src/day1/Sample.java
@@ -1,8 +1,17 @@
 package day1;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class Sample {
 	public static void main(String[] args) {
 		System.out.println("Hello");
 		System.out.println("World");
-	}
 
+	Map<String, Integer> map = new HashMap<>();
+	map.put("東京", 1300);
+	map.put("神奈川", 900);
+	map.put("埼玉", 740);
+	System.out.println(map);
+	}
 }
