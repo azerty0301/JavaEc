80357bdad3a3bdafe2ee53d957b2a1772a142d2b
azerty0301 <morozumiyumie@gmail.com>
2022-05-25 14:27:51 +0900

    2022/05/25追加

---
+++ oldmaid/src/oldmaid/Joker.java
@@ -0,0 +1,10 @@
+package oldmaid;
+
+public class Joker extends Tramp {
+	public Joker() {
+		this("Joker",0,"Joker");
+	}
+	public Joker(String name, int num, String simbol) {
+		super(name, num, simbol);
+	}
+}
